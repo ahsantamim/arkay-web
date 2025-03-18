2063e88c30638ac28e7ebc78c3683c7d9724eefb
ahsanbq <ahsan@konnectingdots.com>
2025-03-18 11:06:50 +0600

    footer bg update

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
           <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
-            <div className="bg-black text-white text-4xl font-extrabold tracking-wide uppercase">
+            <div className=" text-white text-4xl font-extrabold tracking-wide uppercase">
               Arkay
             </div>
 
